b5dfc5ee267ecad3ba4cac471e35ede335e9b40b
pong-pong <pongpong.dev@gmail.com>
2018-08-03 16:20:06 +0900

    [CREATE] JsonObject.java created

---
+++ src/jetpack/annotation/JsonObject.java
@@ -0,0 +1,18 @@
+package jetpack.annotation;
+
+import jetpack.FormatType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author pongpong
+ * @version 1.0
+ * @since 080218
+ */
+
+public @interface JsonObject {
+
+}
