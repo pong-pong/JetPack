3df8c40aa6684a254b37a19fddabbb4efc41ffe5
pong-pong <pongpong.dev@gmail.com>
2018-08-03 16:36:16 +0900

    [CREATE] JsonFormatter.java created

---
+++ src/jetpack/JsonFormatter.java
@@ -0,0 +1,13 @@
+package jetpack;
+
+/**
+ * @author pongpong
+ * @version 1.0
+ * @since 080218
+ */
+
+public class JsonFormatter {
+    public static String toJSON(Object o, FormatType t, boolean nullable) {
+        return null;
+    }
+}
