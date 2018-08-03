89d4be430e416a9b5da9741998f5a606b34586cc
pong-pong <pongpong.dev@gmail.com>
2018-08-03 16:24:57 +0900

    [CREATE] JsonKey.java created

---
+++ src/jetpack/annotation/JsonKey.java
@@ -0,0 +1,16 @@
+package jetpack.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author pongpong
+ * @version 1.0
+ * @since 080218
+ */
+
+public @interface JsonKey {
+
+}
