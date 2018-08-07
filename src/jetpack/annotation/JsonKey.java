16916b2a0f0986fc41bbb5771f0843dd90c2b78e
pong-pong <pongpong.dev@gmail.com>
2018-08-07 20:07:39 +0900

    [UPDATE] @JsonKey annotation target modified

--- src/jetpack/annotation/JsonKey.java
+++ src/jetpack/annotation/JsonKey.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
  * @since 080218
  */
 
-@Target(ElementType.METHOD)
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonKey {
 
